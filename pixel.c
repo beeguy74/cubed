d6209619541644eac87e6236376ef40939656859
Thunderbird Phung <tphung@er-c6.kzn.21-school.ru>
2021-02-17 19:49:25 +0300

    try drow map

--- pixel.c
+++ pixel.c
@@ -6,7 +6,7 @@
 /*   By: tphung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 15:49:52 by tphung            #+#    #+#             */
-/*   Updated: 2021/02/16 19:30:32 by tphung           ###   ########.fr       */
+/*   Updated: 2021/02/17 18:48:00 by tphung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,6 @@ void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
-}
-
-void			square_put(t_data *img, int x, int y, int len)
-{
-	int			i;
-	int			j;
-
-	i = 0;
-	while (i < len)
-	{
-		j = 0;
-		while (j < len)
-		{
-			my_mlx_pixel_put(img, x + i, y + j, 0x00FF0000);
-			j++;
-		}
-		i++;
-	}
 }
 
 void			line_put(t_data *img, int x0, int y0, int x1, int y1)
@@ -85,46 +67,52 @@ int				mouse_hook(int button, int x, int y, t_vars *vars)
 		return (0);
 }
 
-/*int				render_next_frame(t_vars *vars)
+void			square_put(t_data *img, int x, int y, int len)
 {
-	line_put(vars->img, vars->x, vars->y, vars->x1, vars->y1);
-	if (vars->x < 630)
-		vars->x += 20;
-	else
-		vars->x -= 20;
-	mlx_put_image_to_window(vars->mlx, vars->win, vars->img->img, 0, 0);
-	return (1);
-}*/
+	int			i;
+	int			j;
+
+	i = 0;
+	while (i < len)
+	{
+		j = 0;
+		while (j < len)
+		{
+			my_mlx_pixel_put(img, x + i, y + j, 0x00FF0000);
+			j++;
+		}
+		i++;
+	}
+}
+
+int				drow_map(t_vars *vars)
+{
+	vars->y = 0;
+	while (vars->config->link_map[vars->y] != NULL)
+	{
+		vars->x = 0;
+		while (vars->config->link_map[vars->y][vars->x] != '\0')
+		{
+			if (vars->config->link_map[vars->y][vars->x] == '1')
+				square_put(vars->img, vars->x * 10, vars->y * 10, 10);
+			if (ft_strchr("0", vars->config->link_map[vars->y][vars->x]))
+				square_put(vars->img, vars->x * 10, vars->y * 10, 10);
+			vars->x++;
+		}
+		vars->y++;
+	}
+	return (0);
+}
 
 int				render_next_frame(t_vars *vars)
 {
-	square_put(vars->img, vars->x, vars->y, 64);
+/*	square_put(vars->img, vars->x, vars->y, 64);
 	if (vars->x < vars->config->res_x)
-		vars->x += 64;
+		vars->x += 64;*/
+	drow_map(vars);
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->img->img, 0, 0);
 	return (1);
 }
-
-/*int				main(void)
-{
-	t_vars		vars;
-	t_data		img;
-
-	vars.x = 10;
-	vars.y = 10;
-	vars.x1 = 630;
-	vars.y1 = 470;
-	vars.img = &img;
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
-	mlx_key_hook(vars.win, key_hook, &vars);
-	vars.img->img = mlx_new_image(vars.mlx, 640, 480);
-	vars.img->addr = mlx_get_data_addr(vars.img->img, &vars.img->bits_per_pixel,
-			&vars.img->line_length, &vars.img->endian);
-	mlx_loop_hook(vars.mlx, render_next_frame, &vars);
-	mlx_loop(vars.mlx);
-	return (0);
-}*/
 
 int				painting(t_conf *config, t_pers *plr)
 {
@@ -137,14 +125,16 @@ int				painting(t_conf *config, t_pers *plr)
 	vars.y = 0;
 	vars.x1 = 64;
 	vars.y1 = 64;
+
 	vars.img = &img;
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, config->res_y, config->res_x,
 			"Hello world map!");
-	mlx_key_hook(vars.win, key_hook, &vars);
 	vars.img->img = mlx_new_image(vars.mlx, config->res_y, config->res_x);
 	vars.img->addr = mlx_get_data_addr(vars.img->img, &vars.img->bits_per_pixel,
 			&vars.img->line_length, &vars.img->endian);
+
+	mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_loop_hook(vars.mlx, render_next_frame, &vars);
 	mlx_loop(vars.mlx);
 	return (0);
